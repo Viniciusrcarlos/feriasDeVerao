d2484ec017867ca8c2014517827b04b3185ede09
Vinicius Raupp <101226503+Viniciusrcarlos@users.noreply.github.com>
2023-02-03 23:18:01 -0300

    pyy

---
+++ 08_0301/01.py
@@ -0,0 +1,13 @@
+quantidade = int(input('Digite quantos números deseja somar: '))
+
+if quantidade == 2:
+    numero1 = int(input('Digite o primeiro número: '))
+    numero2 = int(input('Digite o segundo número: '))
+    soma = numero1 + numero2
+    print(f'A soma do número {numero1} e {numero2} é: {soma}')
+if quantidade == 3:
+    numero1 = int(input('Digite o primeiro número: '))
+    numero2 = int(input('Digite o segundo número: '))
+    numero3 = int(input('Digite o terceiro número: '))
+    soma = numero1 + numero2 + numero3
+    print(f'A soma entre {numero1}, {numero2} e {numero3} é: {soma}')
