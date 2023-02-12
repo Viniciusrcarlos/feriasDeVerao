550f82a79c60991a5ee7206475721d18f907e6f7
Vinicius Raupp <101226503+Viniciusrcarlos@users.noreply.github.com>
2023-02-12 15:59:38 -0300

    rolex

--- 08_0301/script.js
+++ 08_0301/script.js
@@ -9,7 +9,11 @@ function updateClock(){
     const minute = currentDate.getMinutes()
     const second = currentDate.getSeconds()
     const hourDeg = (hour / 12) * 360;
-    hourEl.style.transform = `rotate(${hourDeg});`
+    hourEl.style.transform = `rotate(${hourDeg}deg)`;
+    const minuteDeg = (minute / 60) * 360;
+    minuteEl.style.transform = `rotate(${minuteDeg}deg)`;
+    const secondDeg = (second / 60) * 360;
+    secondEl.style.transform = `rotate(${secondDeg}deg)`;
 }
 
 updateClock();
