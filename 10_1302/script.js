39d22394c73bc47041d78b6c8b9cbcc3d30f7f75
Vinicius Raupp <101226503+Viniciusrcarlos@users.noreply.github.com>
2023-02-18 21:59:59 -0300

    htmltodo

--- 10_1302/script.js
+++ 10_1302/script.js
@@ -2,36 +2,45 @@
 const todoForm = document.querySelector("#todo-form");
 const todoInput = document.querySelector("#todo-input");
 const todoList = document.querySelector("#todo-list");
-const editForn = document.querySelector("#edit-form");
+const editForm = document.querySelector("#edit-form");
 const editInput = document.querySelector("#edit-input");
 const cancelEditBtn = document.querySelector("#cancel-edit-btn");
 
 // Funções
 const saveTodo = (text) => {
-    consttodo = document.createElement("div")
-    todoForm.classList.add("todo")
+    const todo = document.createElement("div");
+    todo.classList.add("todo");
 
-    const todoTitle = document.createElement("h3")
-    todoTitle.innerText = text
-    todoForm.appendChild(todoTitle)
+    const todoTitle = document.createElement("h3");
+    todoTitle.innerText = text;
+    todo.appendChild(todoTitle);
 
-    const doneBtn = document.createElement("button")
-    doneBtn.classList.add("finish-todo")
-    doneBtn.innerHTML = '<i class="fa-solid fa-check"></i>'
-    todoForm.appendChild(doneBtn)
+    const doneBtn = document.createElement("button");
+    doneBtn.classList.add("finish-todo");
+    doneBtn.innerHTML = '<i class="fa-solid fa-check"></i>';
+    todo.appendChild(doneBtn);
 
-    const editBtn = document.createElement("button")
-    editBtn.classList.add("edit-todo")
-    editBtn.innerHTML = '<i class="fa-solid fa-pen"></i>'
-    todoForm.appendChild(editBtn)
+    const editBtn = document.createElement("button");
+    editBtn.classList.add("edit-todo");
+    editBtn.innerHTML = '<i class="fa-solid fa-pen"></i>';
+    todo.appendChild(editBtn);
 
-    const deleteBtn = document.createElement("button")
-    deleteBtn.classList.add("remove-todo")
-    deleteBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>'
-    todoForm.appendChild(deleteBtn)
+    const deleteBtn = document.createElement("button");
+    deleteBtn.classList.add("remove-todo");
+    deleteBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+    todo.appendChild(deleteBtn);
 
     todoList.appendChild(todo)
+
+    todoInput.value = "";
+    todoInput.focus();
 };
+
+const toggleForms = () => {
+    editForm.classList.toggle("hide")
+    todoForm.classList.toggle("hide")
+    todoList.classList.toggle("hide")
+}
 
 // Eventos
 todoForm.addEventListener("submit", (e) => {
@@ -43,3 +52,22 @@ todoForm.addEventListener("submit", (e) => {
         saveTodo(inputValue)
     }
 });
+
+document.addEventListener("click", (e) => {
+    const targetEl = e.target;
+    const parentEl = targetEl.closest("div");
+
+    if(targetEl.classList.contains("finish-todo")){
+        parentEl.classList.toggle("done")
+    }
+
+    if(targetEl.classList.contains("remove-todo")){
+        parentEl.remove();
+    }
+
+    if(targetEl.classList.contains("edit-todo")){
+        toggleForms()
+    }
+});
+
+cancelEditBtn.addEventListener("click", )
