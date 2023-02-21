c94318e0b56cad717c165a33758895da862115e7
Vinicius Raupp <101226503+Viniciusrcarlos@users.noreply.github.com>
2023-02-21 19:30:59 -0300

    hmtl

--- 02.py
+++ 02.py
@@ -1,9 +1,7 @@
 #Desenvolva um programa que tenha uma função que verifique se um número inteiro qualquer é par ou impar
 
 numero = int(input('Digite um número: '))
-
 parImpar = numero%2
-
 if parImpar == 0:
     print('Par')
 else:
